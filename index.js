cfe58b1d4a179836c1a909713b049a52fd4e08ec
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-11-28 02:32:36 +0100

    Comment out missing templates

--- index.js
+++ index.js
@@ -21,38 +21,38 @@ const {
 const cwd = process.cwd()
 
 const FRAMEWORKS = [
-  {
-    name: 'vanilla',
-    color: yellow,
-    variants: [
-      {
-        name: 'vanilla',
-        display: 'JavaScript',
-        color: yellow
-      },
-      {
-        name: 'vanilla-ts',
-        display: 'TypeScript',
-        color: blue
-      }
-    ]
-  },
-  {
-    name: 'vue',
-    color: green,
-    variants: [
-      {
-        name: 'vue',
-        display: 'JavaScript',
-        color: yellow
-      },
-      {
-        name: 'vue-ts',
-        display: 'TypeScript',
-        color: blue
-      }
-    ]
-  },
+  // {
+  //   name: 'vanilla',
+  //   color: yellow,
+  //   variants: [
+  //     {
+  //       name: 'vanilla',
+  //       display: 'JavaScript',
+  //       color: yellow
+  //     },
+  //     {
+  //       name: 'vanilla-ts',
+  //       display: 'TypeScript',
+  //       color: blue
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: 'vue',
+  //   color: green,
+  //   variants: [
+  //     {
+  //       name: 'vue',
+  //       display: 'JavaScript',
+  //       color: yellow
+  //     },
+  //     {
+  //       name: 'vue-ts',
+  //       display: 'TypeScript',
+  //       color: blue
+  //     }
+  //   ]
+  // },
   {
     name: 'react',
     color: cyan,
@@ -62,61 +62,61 @@ const FRAMEWORKS = [
         display: 'JavaScript',
         color: yellow
       },
-      {
-        name: 'react-ts',
-        display: 'TypeScript',
-        color: blue
-      }
+      // {
+      //   name: 'react-ts',
+      //   display: 'TypeScript',
+      //   color: blue
+      // }
     ]
   },
-  {
-    name: 'preact',
-    color: magenta,
-    variants: [
-      {
-        name: 'preact',
-        display: 'JavaScript',
-        color: yellow
-      },
-      {
-        name: 'preact-ts',
-        display: 'TypeScript',
-        color: blue
-      }
-    ]
-  },
-  {
-    name: 'lit',
-    color: lightRed,
-    variants: [
-      {
-        name: 'lit',
-        display: 'JavaScript',
-        color: yellow
-      },
-      {
-        name: 'lit-ts',
-        display: 'TypeScript',
-        color: blue
-      }
-    ]
-  },
-  {
-    name: 'svelte',
-    color: red,
-    variants: [
-      {
-        name: 'svelte',
-        display: 'JavaScript',
-        color: yellow
-      },
-      {
-        name: 'svelte-ts',
-        display: 'TypeScript',
-        color: blue
-      }
-    ]
-  }
+  // {
+  //   name: 'preact',
+  //   color: magenta,
+  //   variants: [
+  //     {
+  //       name: 'preact',
+  //       display: 'JavaScript',
+  //       color: yellow
+  //     },
+  //     {
+  //       name: 'preact-ts',
+  //       display: 'TypeScript',
+  //       color: blue
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: 'lit',
+  //   color: lightRed,
+  //   variants: [
+  //     {
+  //       name: 'lit',
+  //       display: 'JavaScript',
+  //       color: yellow
+  //     },
+  //     {
+  //       name: 'lit-ts',
+  //       display: 'TypeScript',
+  //       color: blue
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: 'svelte',
+  //   color: red,
+  //   variants: [
+  //     {
+  //       name: 'svelte',
+  //       display: 'JavaScript',
+  //       color: yellow
+  //     },
+  //     {
+  //       name: 'svelte-ts',
+  //       display: 'TypeScript',
+  //       color: blue
+  //     }
+  //   ]
+  // }
 ]
 
 const TEMPLATES = FRAMEWORKS.map(
